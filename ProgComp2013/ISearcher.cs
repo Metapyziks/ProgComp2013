ed8ae37869905ca7ae8e0c5aff7f57e7b2efda55
James King <metapyziks@gmail.com>
2013-11-04 20:16:06 +0000

    Commented Tools and ISearcher

--- ProgComp2013/ISearcher.cs
+++ ProgComp2013/ISearcher.cs
@@ -1,5 +1,9 @@
 ﻿namespace ProgComp2013
 {
+    /// <summary>
+    /// Exposes the Search method, which generates a Route
+    /// from a given Map.
+    /// </summary>
     public interface ISearcher
     {
         Route Search(Map map, int maxLength);
--- ProgComp2013/Tools.cs
+++ ProgComp2013/Tools.cs
@@ -2,8 +2,17 @@
 
 namespace ProgComp2013
 {
+    /// <summary>
+    /// Assorted helper and extension methods.
+    /// </summary>
     public static class Tools
     {
+        /// <summary>
+        /// Finds the position offset that would be the result of
+        /// travelling in the specified direction.
+        /// </summary>
+        /// <param name="dir">Direction of the offset.</param>
+        /// <returns>Position offset that a given direction represents.</returns>
         public static Point Normal(this Direction dir)
         {
             switch (dir) {
