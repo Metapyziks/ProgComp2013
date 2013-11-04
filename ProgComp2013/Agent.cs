d7eb4f9ac11a325a4a7729e2ccdfae4023531a0a
James King <metapyziks@gmail.com>
2013-11-04 20:24:40 +0000

    Commented Agent

--- ProgComp2013/Agent.cs
+++ ProgComp2013/Agent.cs
@@ -2,6 +2,10 @@
 
 namespace ProgComp2013
 {
+    /// <summary>
+    /// Represents an entity that follows a route across a given map,
+    /// and keeps track of the combined probabilities of all visited tiles.
+    /// </summary>
     public class Agent : IEnumerator<Direction>
     {
         private IEnumerator<Direction> _dirs;
@@ -9,27 +13,50 @@ namespace ProgComp2013
 
         private Map _workingMap;
 
+        /// <summary>
+        /// Current horizontal position of the agent.
+        /// </summary>
         public int X { get; private set; }
+
+        /// <summary>
+        /// Current vertical position of the agent.
+        /// </summary>
         public int Y { get; private set; }
 
+        /// <summary>
+        /// Current accumulated probability of the agent.
+        /// </summary>
         public double Score { get; private set; }
 
+        /// <summary>
+        /// Creates a new agent that follows the specified route
+        /// across the given map.
+        /// </summary>
+        /// <param name="map">Map to traverse.</param>
+        /// <param name="dirs">Route to follow.</param>
         public Agent(Map map, IEnumerable<Direction> dirs)
+            : this(map, dirs.GetEnumerator()) { }
+        
+        /// <summary>
+        /// Creates a new agent that follows the specified route
+        /// across the given map.
+        /// </summary>
+        /// <param name="map">Map to traverse.</param>
+        /// <param name="dirIter">Next direction selection enumerator.</param>
+        public Agent(Map map, IEnumerator<Direction> dirIter)
         {
-            _dirs = dirs.GetEnumerator();
+            _dirs = dirIter;
             _originalMap = map;
 
             Reset();
         }
 
+        /// <summary>
+        /// The direction the agent last moved in.
+        /// </summary>
         public Direction Current
         {
             get { return _dirs.Current; }
-        }
-
-        public void Dispose()
-        {
-            _dirs.Dispose();
         }
 
         object System.Collections.IEnumerator.Current
@@ -45,8 +72,15 @@ namespace ProgComp2013
             Y += normal.Y;
 
             Score += _workingMap[X, Y];
+
+            _workingMap[X, Y] = 0.0;
         }
 
+        /// <summary>
+        /// Advances the agent to the next step in its route.
+        /// </summary>
+        /// <returns>True if the agent has not reached the end of
+        /// its route, false otherwise.</returns>
         public bool MoveNext()
         {
             if (_dirs.MoveNext()) {
@@ -57,6 +91,9 @@ namespace ProgComp2013
             }
         }
 
+        /// <summary>
+        /// Resets the agent to its initial state.
+        /// </summary>
         public void Reset()
         {
             _dirs.Reset();
@@ -64,6 +101,15 @@ namespace ProgComp2013
             _workingMap = _originalMap.Clone();
 
             Score = 0.0;
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing,
+        /// or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            _dirs.Dispose();
         }
     }
 }
