d5f11a1e0d4f5817a8814c34e66451247471302b
James King <metapyziks@gmail.com>
2013-11-04 21:08:13 +0000

    Commented Route and added IterativeSearcher

---
+++ ProgComp2013/IterativeSearcher.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace ProgComp2013
+{
+    /// <summary>
+    /// Abstract base class for searchers that iteratively moves an agent
+    /// to create a route.
+    /// </summary>
+    public abstract class IterativeSearcher : ISearcher
+    {
+        private Agent _agent;
+
+        /// <summary>
+        /// Method called when a new route is about to be searched.
+        /// </summary>
+        protected virtual void OnBegin() { }
+
+        /// <summary>
+        /// Method to decide what direction an agent should travel in order
+        /// to produce a route.
+        /// </summary>
+        /// <param name="agent">Agent to instruct.</param>
+        /// <returns>The direction the agent should move next step.</returns>
+        protected abstract Direction Next(Agent agent);
+
+        private IEnumerator<Direction> Enumerator()
+        {
+            for(;;) {
+                var dir = Next(_agent);
+                if (dir == Direction.None) yield break;
+                yield return dir;
+            }
+        }
+
+        /// <summary>
+        /// Generates a route that traverses the given map within the specified
+        /// number of steps.
+        /// </summary>
+        /// <param name="map">Map to traverse.</param>
+        /// <param name="maxLength">Maximum number of steps to take.</param>
+        /// <returns>A route that traverses the given map.</returns>
+        public Route Search(Map map, int maxLength = Map.Width * Map.Height)
+        {
+            _agent = new Agent(map, Enumerator());
+
+            OnBegin();
+
+            var route = new Route();
+            for (int i = 0; i < maxLength && _agent.MoveNext(); ++i) {
+                route.Add(_agent.Current);
+            }
+
+            return route;
+        }
+    }
+}
--- ProgComp2013/Route.cs
+++ ProgComp2013/Route.cs
@@ -1,35 +1,58 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ProgComp2013
 {
+    /// <summary>
+    /// Enumeration of the cardinal directions.
+    /// </summary>
     public enum Direction
     {
+        None = 0,
         North = 1,
         East = 2,
         South = 3,
         West = 4
     }
 
+    /// <summary>
+    /// Respresents a series of directions taken to traverse a map.
+    /// </summary>
     public class Route : IEnumerable<Direction>
     {
         private List<Direction> _dirs;
 
+        /// <summary>
+        /// Creates a new route with an empty list of directions.
+        /// </summary>
         public Route()
         {
             _dirs = new List<Direction>();
         }
 
+        /// <summary>
+        /// Creates a new route with a specified initial list of directions.
+        /// </summary>
+        /// <param name="dirs">Initial direction sequence.</param>
         public Route(IEnumerable<Direction> dirs)
         {
             _dirs = dirs.ToList();
         }
 
+        /// <summary>
+        /// Appends a direction to the route.
+        /// </summary>
+        /// <param name="dir">Direction to append.</param>
         public void Add(Direction dir)
         {
             _dirs.Add(dir);
         }
 
+        /// <summary>
+        /// Returns an enumerator that iterates through the route.
+        /// </summary>
+        /// <returns>An enumerator that iterates through the route.</returns>
         public IEnumerator<Direction> GetEnumerator()
         {
             return _dirs.GetEnumerator();
@@ -40,16 +63,24 @@ namespace ProgComp2013
             return _dirs.GetEnumerator();
         }
 
+        /// <summary>
+        /// Finds an objective score representing the quality of
+        /// the route as a value between 0.0 and 1.0.
+        /// </summary>
+        /// <param name="map">The map to evaluate the route on.</param>
+        /// <returns>A score between 0.0 and 1.0.</returns>
         public double CalculateScore(Map map)
         {
             var agent = new Agent(map, this);
             double score = 0.0;
+            int moves = 0;
 
             while (agent.MoveNext()) {
                 score += agent.Score;
+                ++moves;
             }
 
-            return score;
+            return score / Math.Max(10000, moves);
         }
     }
 }
